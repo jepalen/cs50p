e264bdf0074fb2a8b3b4e4379804548d7ba42533
jepalen <jepalen@gmail.com>
2025-10-31 10:52:19 +0100

    first lesson finished with details

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # cs50p
 python Harvard course
+    lection_0: Functions, Variables
 
 # Jeff P. 
 information contained here is, and remains mine and is for education purpose, feel free to see and use it, it is code for learning the cs50P course, but don't be lazy 
--- lection_0/hello.py
+++ lection_0/hello.py
@@ -8,14 +8,18 @@ hello world program
 
 # Basic Documentation : docs.python.org/3/library/index.html
 
+emogi = ":()" #global variable
+
 def hello(name="world 2"):
-    print("Welcome to this \"new\" course", end=' ;) \n') #rewrite end parameter to avoid newline
+    global emogi #access global variable inside function
+    print("Welcome to this \"new\" course "+ emogi, end=' ;) \n') #rewrite end parameter to avoid newline
     #remove leading/trailing whitespace, capitalize first letter of each word,
     name=name.strip().capitalize().title() 
 
     #split into first and last name
     first, last = name.split(" ")
-    print("hello world, " + "it's me", first )
+    emogi = " :D "
+    print("hello world, " + "it's me" + emogi, first )
     print(f"{last} is a nice lastname" ) #f-string version
 
 
