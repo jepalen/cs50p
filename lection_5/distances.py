4e11308d8861a218137c5b827e0ebeb49389052b
jepalen <jepalen@gmail.com>
2025-11-20 07:26:20 +0100

    6th lesson with  black finished

--- lection_5/distances.py
+++ lection_5/distances.py
@@ -5,27 +5,24 @@ distances = {
     "Columbia": "400 AU",
 }
 
-
-def convert_to_m(au):
-    if not isinstance(au, (int, float)):
-        raise TypeError("eu most be int or flat")
-    return au * 149597870691.7
-
+def convert_to_km(au):
+    if not isinstance(au,(int,float)):
+        raise TypeError('eu most be int or flat')
+    return au * 149597870.7
 
 def main():
-    spacecraft = input("Select the spacecraft: ")
+    spacecraft = input('Select the spacecraft: ')
     try:
-        au = float(distances[spacecraft])
+        au= float(distances[spacecraft])
     except KeyError:
         print(f'the spacecraft "{spacecraft}" is not in the dictionary')
         return
-    except ValueError:
-        print(f'the value: "{distances[spacecraft]}" cant be converted to float')
-        return
+    except ValueError:  
+        print(f'the value: "{distances[spacecraft]}" cant be converted to float') 
+        return 
 
     km = convert_to_km(au)
-    print(f"the distance in km for {spacecraft} is {km}")
+    print(f'the distance in km for {spacecraft} is {km}')
 
-
-if __name__ == "__main__":
+if __name__=='__main__':  
     main()
